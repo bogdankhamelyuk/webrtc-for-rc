1440d3e495d2555dd0019f999636b9d3d264df35
Bogdan Khamelyuk <bogdankhamelyuk@gmail.com>
2024-02-24 23:18:41 +0100

    added event-target-shim to avoid errors on webrtc

---
+++ screens/main.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import MediaStream from "react-native-webrtc";
+
+export default function MainScreen() {
+  //   const [remoteStream, setRemoteStream] = useState(null);
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
